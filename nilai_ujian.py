410bb88b065eae27db730b2c9f971ef2f5f68979
Muh. Naufal Bimasbuqin <mnaufalbmsbqn@gmail.com>
2024-10-21 02:48:17 +0800

    Create nilai_ujian.py

---
+++ nilai_ujian.py
@@ -0,0 +1,11 @@
+print("mengetahu nilai ujian kalian")
+nilai_ujian = float (input("masukan nilai anda: "))
+
+if nilai_ujian >=80:
+    print ("A")
+if nilai_ujian >=70 and nilai_ujian <80 :
+    print ("B")
+if nilai_ujian >=60 and nilai_ujian <70 :
+    print ("C")
+if nilai_ujian<60 :
+    print ("TIDAK LULUS")
